1ee12d190e1785b9150f23db1d467692846e1e2a
julio <julio.sanic.gt.256@gmail.com>
2024-01-16 06:23:37 -0600

    button event added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState<string[]>([])
+
+  const goalInputHandler = (enteredText: string) => {
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>  
+
+        <View style={styles.textContainer}>
+        <TextInput 
+          placeholder='your course goal!'
+          placeholderTextColor={'#dedede'}
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+        />
+        </View> 
+
+        <View style={styles.buttonContainer}>
+          <TouchableWithoutFeedback
+            onPress={addGoalHandler}
+          >
+            <View style={{backgroundColor: '#dedede', padding: 10, borderRadius: 25}}>
+              <Text 
+                style={{
+                  color: 'black',
+                  textAlign: 'center',
+                }
+              }>
+                ADD
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+
+      </View>
+
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal, index) => (
+          <Text key={index}>
+            {goal}
+          </Text>
+        ))}
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    paddingHorizontal: 16,
+    backgroundColor: '#2e2e2e',
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomColor: 'rgba(255, 255, 255, 0.3)',
+    borderBottomWidth: 1,
+    borderStyle: 'solid',
+    paddingBottom: 24
+  },
+  textInput: {
+    borderBottomColor: '#dedede',
+    borderBottomWidth: 1,
+    color: '#dedede',
+  },
+  textContainer: {
+    width: '65%'
+  },
+  buttonContainer: {
+    width: '25%',
+  },
+  goalsContainer: {
+    flex: 5
+  }
 });
